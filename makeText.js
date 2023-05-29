986607378c71438797718ff3dfe8498b19f277b0
lietkynes <mdhill387@gmail.com>
2023-05-29 19:47:12 -0400

    first commit markov, issues with constructor function

---
+++ makeText.js
@@ -0,0 +1,51 @@
+/** Command-line tool to generate Markov text. */
+
+const markov = require('./markov')
+const fs = require('fs')
+const process = require('process')
+const axios = require('axios')
+
+
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text)
+    console.log(mm.makeText());
+}
+
+function makeText(path) {
+    fs.readFile(path, 'utf8', function cb(err, data) {
+        if (err) {
+            console.log('ERROR', err)
+            process.exit(1);
+        }
+        else {
+            generateText(data);
+        }
+    });
+}
+
+async function makeURLText(url) {
+    let resp;
+
+    try {
+        resp = await axios.get(url);
+    }
+    catch (err) {
+        console.error('ERROR', err)
+        process.exit(1)
+    }
+}
+
+let [method, path] = process.argv.slice(2);
+
+if (method === 'file') {
+    makeText(path)
+}
+
+else if (method === 'url') {
+    makeURLText(path)
+}
+
+else {
+    console.error('Unknown Method')
+    process.exit(1)
+}
